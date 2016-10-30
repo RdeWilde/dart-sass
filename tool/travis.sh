aa9a078a56d7d53a8bf4fff8c3f33d10f3331cc5
Natalie Weizenbaum <nex342@gmail.com>
2016-10-30 12:54:24 -0700

    Add travis infrastructure that might not work yet.

---
+++ tool/travis.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Copyright 2016 Google Inc. Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+# Emit folding annotations on Travis, or just newlines elsewhere.
+fold ()
+{
+    if [ "$TRAVIS" = true ]; then
+        echo "travis_fold:start:$1"
+    fi
+    "${@:2}"
+    if [ "$TRAVIS" = true ]; then
+        echo "travis_fold:end:$1"
+    else
+        echo
+        echo
+    fi
+}
+
+dart_sass=`pwd`
+dir=`mktemp -d /tmp/sass-spec-XXXXXXXX`
+cd "$dir"
+
+fold "git clone sass-spec" \
+     git clone file:///home/nweiz/code/sass-spec --branch dart-sass --depth 1
+cd sass-spec
+
+fold "bundle install" bundle install --jobs=3 --retry=3
+bundle exec sass-spec.rb --output-style expanded --probe-todo --dart "$dart_sass"
+cd "$dart_sass"
+rm -rf "$dir"
