71554e9039b86d1b3731babe4e0789c3fde02300
Natalie Weizenbaum <nex342@gmail.com>
2016-10-31 14:29:41 -0700

    Fail on script errors.

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 # Copyright 2016 Google Inc. Use of this source code is governed by an MIT-style
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
